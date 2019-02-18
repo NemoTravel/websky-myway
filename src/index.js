173f4515ee7bf0a288a9575f45a89524e1d4302a
Evgeny <Litvinenko>
2019-02-18 17:45:40 +0400

    add right days names

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-require('./datepicker.conf.js');
+moment.locale('ka', {
+    weekdaysShort : 'ორშ._სამშ._ოთხ._ხუთ._პარ._შაბ._კვი.'.split('_'),
+    weekdaysMin : 'ორშ_სამშ_ოთხ_ხუთ_პარ_შაბ_კვი'.split('_'),
+});
